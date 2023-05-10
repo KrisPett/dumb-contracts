8effeab3cc3f9fac07661f2ca05bed10143a8c41
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-05-10 20:45:14 +0200

    commit

--- ethereum/chaincue-nfts/app/scripts/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const {TOKEN_URI, CONTRACT_ADDRESS, PUBLIC_ADDRESS} = process.env
-
-const Web3 = require('web3');
-
-// Replace the providerURL with the actual Sepolia network's RPC URL
-const providerURL = '<providerURL>';
-const web3 = new Web3(providerURL);
