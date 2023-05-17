973e857b189b3ac93b33a276fcc87bf8f93db00b
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-05-17 20:15:20 +0200

    commit

--- ethereum/chaincue-nfts/app/src/types/environment.d.ts
+++ ethereum/chaincue-nfts/app/src/types/environment.d.ts
@@ -1,14 +1,5 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NEXT_PUBLIC_CLIENT_URL: string
-    NEXT_PUBLIC_AWS_GATEWAY_URL_CHATBOT: string
-    NEXT_PUBLIC_AWS_GATEWAY_URL_IMAGEBOT: string
-    NEXT_PUBLIC_AWS_GATEWAY_URL_IMAGEBOT_FILTER_IMAGES: string
-    NEXT_PUBLIC_WALLET_CONNECT_ID: string
-    NEXTAUTH_URL: string
-    NEXTAUTH_SECRET: string
-    KEYCLOAK_ID: string
-    KEYCLOAK_SECRET: string
-    KEYCLOAK_ISSUER: string
+    NEXT_PUBLIC_INFURA_API_KEY: string
   }
 }
