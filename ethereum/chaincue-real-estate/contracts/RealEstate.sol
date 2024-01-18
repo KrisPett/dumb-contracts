e5f3bd381de31a1c50bf841c3eb3de2bf95ee1dc
KQT3 <70701718+KQT3@users.noreply.github.com>
2024-01-18 20:05:44 +0100

    commit

--- ethereum/chaincue-real-estate/contracts/RealEstate.sol
+++ ethereum/chaincue-real-estate/contracts/RealEstate.sol
@@ -1,46 +1,39 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RealEstate is ERC721Enumerable, Ownable {
-    uint256 public propertyCount;
-    mapping(uint256 => Property) public properties;
+contract RealEstate is ERC721URIStorage, Ownable {
+    uint256 public nextTokenId;
+    mapping(uint256 => PropertyDetails) public propertyDetails;
 
-    struct Property {
+    struct PropertyDetails {
+        string name;
         string location;
-        uint256 price;
-        bool isForSale;
+        uint256 size;
     }
 
-    event PropertyRegistered(uint256 propertyId, string location, uint256 price);
-    event PropertyTransferred(uint256 propertyId, address from, address to);
+    event PropertyMinted(uint256 indexed tokenId, address indexed owner, PropertyDetails details);
 
-//    constructor() ERC721("RealEstateNFT", "RE-NFT") {}
-    constructor(address _initialOwner) ERC721("RealEstateNFT", "RE-NFT") Ownable(_initialOwner) {}
+    constructor() ERC721("RealEstate", "RET") {}
 
+    function mintProperty(string memory name, string memory location, uint256 size, string memory tokenURI) public onlyOwner {
+        uint256 tokenId = nextTokenId;
+        nextTokenId++;
 
-    function registerProperty(string memory _location, uint256 _price) external onlyOwner {
-        uint256 propertyId = propertyCount + 1;
-        _safeMint(msg.sender, propertyId);
-        properties[propertyId] = Property({
-            location: _location,
-            price: _price,
-            isForSale: true
-        });
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURI);
 
-        emit PropertyRegistered(propertyId, _location, _price);
-        propertyCount++;
+        PropertyDetails memory details = PropertyDetails(name, location, size);
+        propertyDetails[tokenId] = details;
+
+        emit PropertyMinted(tokenId, msg.sender, details);
     }
 
-    function transferProperty(uint256 _propertyId, address _to) external {
-        require(ownerOf(_propertyId) == msg.sender, "You are not the owner");
-        require(properties[_propertyId].isForSale, "Property is not for sale");
-
-        _transfer(msg.sender, _to, _propertyId);
-        properties[_propertyId].isForSale = false;
-
-        emit PropertyTransferred(_propertyId, msg.sender, _to);
+    function transferProperty(address to, uint256 tokenId) public {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
+        _transfer(msg.sender, to, tokenId);
     }
 }
+
--- ethereum/chaincue-real-estate/scripts/deploy.ts
+++ ethereum/chaincue-real-estate/scripts/deploy.ts
@@ -1,9 +1,26 @@
+// import {ethers} from "hardhat";
+//
+// async function main() {
+//   const contract = await ethers.getContractFactory("RealEstate");
+//   const initialOwnerAddress = 'YOUR_INITIAL_OWNER_ADDRESS';
+//   const contractDeployed = await contract.deploy(initialOwnerAddress, {from: initialOwnerAddress});
+//
+//   await contractDeployed.waitForDeployment().then(value => console.log(value))
+//
+//   console.log(`contractDeployed deployed to: ${await contractDeployed.getAddress()}`);
+// }
+//
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
 import {ethers} from "hardhat";
 
 async function main() {
   const contract = await ethers.getContractFactory("RealEstate");
-  const initialOwnerAddress = 'YOUR_INITIAL_OWNER_ADDRESS';
-  const contractDeployed = await contract.deploy(initialOwnerAddress, {from: initialOwnerAddress});
+  const initialOwnerAddress = '0xBA5deA80566bCB1d79ec380fF4aCde3815a09c8b';
+  const contractDeployed = await contract.deploy(initialOwnerAddress);
 
   await contractDeployed.waitForDeployment().then(value => console.log(value))
 
